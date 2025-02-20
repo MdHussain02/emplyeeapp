ba3944de06be2e9cab5a3f75a23a45dc98923961
Muhammad Hussain <85096977+MdHussain02@users.noreply.github.com>
2025-02-20 15:10:17 +0530

    Update Protected.jsx

--- src/components/Auth/Protected.jsx
+++ src/components/Auth/Protected.jsx
@@ -1,6 +1,6 @@
 import { useAtom } from "jotai"; // Import useAtom from Jotai
 import { Navigate } from "react-router-dom";
-import { userState } from "../../jotai/userState"; // Import the Jotai user state atom
+import { userState } from "../../jotai/userState";
 
 const Protected = ({ children }) => {
   const [user] = useAtom(userState); // Use Jotai's useAtom to access the user state
